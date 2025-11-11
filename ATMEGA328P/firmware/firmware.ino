7bf8ad8bf9437d508bcbfee7778bee80a61e5a0b
lilleole <130458699+lilleole@users.noreply.github.com>
2025-11-11 13:21:56 +0100

    Implemented uart command handling and sensor data retrieval
    Added functionality for handling uart commands and sensor data.

--- ATMEGA328P/firmware/firmware.ino
+++ ATMEGA328P/firmware/firmware.ino
@@ -1,6 +1,11 @@
 #include <SoftwareSerial.h>
 
 
+MKRIoTCarrier carrier;
+
+float acc_x, acc_y, acc_z;
+float gy_x, gy_y, gy_z;
+
 constexpr uint8_t PIN_RX = 2;
 constexpr uint8_t PIN_TX = 3;
 
@@ -8,10 +13,19 @@ const uint8_t syn = 0x53;
 const uint8_t synAck = 0x54;
 const uint8_t ack = 0x55;
 
+
 SoftwareSerial uart(2, 3);
+
+int speed = 0;
 
 
 void setup(){
+  carrier.noCase();
+  carrier.begin();
+  
+  carrier.Relay1.open();
+  carrier.Relay2.open();
+
   constexpr uint8_t PIN_RX = 2;
   constexpr uint8_t PIN_TX = 3;
 
@@ -26,9 +40,64 @@ void setup(){
   Serial.println("Handshake succeeded");
 }
 
-void loop() {}
+void loop() {
+  [byte] cmd = uart.read();
+  switch (cmd[0]) {
+    case 0x61:
+      setSpeed(cmd);
+      break;
+    case 0x62:
+      break;
+    case 0x63:
+      sensorRequset(cmd);
+    break;
+  }
+}
+
+[floats] sensorRequset([byte] cmd){
+switch (cmd[1]) {
+    case 0x01:
+      accData();
+      break;
+    case: 0x02:
+      gyroData();
+      break;
+    case 0x03:
+      MoistMeterData();
+      break;
+    case: 0x04:
+      tempData();
+      break;
+    case 0x03:
+      pressureData();
+      break;
+    
+    default:
+      break;
+
+  }
+}
+
+
+void setSpeed ([byte] cmd) {
+  switch (cmd[1]) {
+    case 0x01:
+      speed = 0;
+      break;
+    case: 0x02:
+      speed = 50;
+      break;
+    case 0x03:
+      speed = 100;
+      break;
+    default:
+      break;
+
+  }
+}
 
 bool performHandshake() {
+
   bool handshakeInitialised = false;
   Serial.println("Awaiting handshake init...");
 
@@ -56,3 +125,24 @@ bool performHandshake() {
   }
   return false;
 }
+
+
+void MoistMeterData() {uart.write(carrier.Env.readHumidity());}
+void tempData() {uart.write(carrier.Env.readTemperature());}
+void pressureData() {uart.write(carrier.Pressure.readPressure());}
+void accData() {
+  if (carrier.IMUmodule.accelerationAvailable())
+    {
+      carrier.IMUmodule.readAcceleration(acc_x, acc_y, acc_z);
+      uart.write(acc_x);
+    }
+}
+
+void gyroData() {
+  if (carrier.IMUmodule.gyroscopeAvailable())
+    {
+      carrier.IMUmodule.readGyroscope(gy_x, gy_y, gy_z);
+      uart.write(gy_x);
+    }
+}
+
